03a7a5e9e13474b6e7366feb63dd667e584ae3f5
TKshuhei <takamatushuhei@outlook.jp>
2020-09-13 00:59:09 +0900

    ボール生成

---
+++ Assets/Scripts/BallControllerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControllerScript : MonoBehaviour
+{
+    public GameObject[] balls;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Button1Pushed() {
+        int random = 0;
+        Instantiate(balls[random],  new Vector3(-2.5f,2f,-0.75f), Quaternion.identity);
+    }
+
+    public void Button2Pushed() {
+        
+    }
+}
